e3de22b837c9a49995aa80938a39efa006407056
wahyusambas <60055494+wahyusambas@users.noreply.github.com>
2020-01-25 12:48:29 +0700

    Add files via upload

---
+++ beranda_adm.php
@@ -0,0 +1,38 @@
+<div class="container">
+    <div class="row">
+        <div class ="col-xs-12">
+
+            <div class="alert alert-info">
+                Selamat datang kembali <strong><?=$_SESSION['nama']?></strong>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <!--colomn kedua-->
+        <div class="col-sm-12 col-xs-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Halaman Administrator Sistem Informasi Data Penduduk Kecamatan SEI Balai</h3>
+                </div>
+                <div class="panel-body">
+                     <table id="dtskripsi" class="table table-bordered table-striped table-hover">
+                       <thead>
+                          <p align="center"><img src="img/logo.jpg"width="250px" height="320px"></img></p>
+                          <p class="label-title" align="center"><strong>DATA PENDUDUK KECAMATAN SEI BALAI</strong></p>
+                          <p class="col-sm-12 col-xs-12" align="center">
+                            Selamat datang di halaman administrator sistem informasi pengolahan Data Penduduk kecamatan sei Balai.<br>
+                            Melalui halaman ini dapat dilakukan pengelolaan data penduduk,<br><br>
+                            Akses menu Master Data pada bagian atas sistem untuk pengelolaan data penduduk.<br>
+                            Untuk mengelola laporan, dapat dilakukan dengan mengakses menu report pada bagian atas sistem.<br>
+                            Akses menu User untuk mengelola informasi tentang user yang login.<br><br><br>
+
+                          </p>
+
+                         </thead
+
+
+                    </table>
+                </div>
+            </div>
+        </div>
+</div>
